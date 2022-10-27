ab45ca6d5179a65f7b68cca1056046fd5088b4e5
Hanif Dadzie <oceanplacid@gmail.com>
2022-10-27 20:38:51 +0000

    CRUD commit

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Artiste, Song, Lyrics
 
-# Register your models here.
+class SongAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register (Artiste, SongAdmin)
+admin.site.register (Song, SongAdmin)
+admin.site.register (Lyrics, SongAdmin)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.template.defaultfilters import default
+from datetime import datetime
 
-# Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length = 50, blank=False, null = False)
+    last_name = models.CharField(max_length = 50, blank = True, null = True)
+    age = models.IntegerField(blank=False, null = False)
+    
+    def __str__(self):
+        return self.first_name
+
+class Song (models.Model):
+    title = models.CharField(max_length = 100, blank=False, null = False)
+    date_released = models.DateTimeField ()
+    likes = models.IntegerField ()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+class Lyrics (models.Model):
+    content = models.TextField(max_length = 20000, blank = False, null = False)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.content
+    
+    
